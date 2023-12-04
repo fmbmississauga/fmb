a548912e81e6121bbcbe69d10b9c2c9ce683d469
Ismail Raja <thismail.raja@gmail.com>
2023-12-04 11:36:04 -0500

    main: 25

--- Scripts/Fateha Form/Code.js
+++ Scripts/Fateha Form/Code.js
@@ -1,11 +1,11 @@
 function onFormSubmit(e) {
-  var responseSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Form Responses 1"); // Replace with the actual name of your response sheet
+  var responseSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Form Responses 5"); // Replace with the actual name of your response sheet
   var lastRow = responseSheet.getLastRow();
   var questionsRange = responseSheet.getRange(1, 1, 1, responseSheet.getLastColumn());
   var questions = questionsRange.getValues()[0];
   var responses = responseSheet.getRange(lastRow, 1, 1, responseSheet.getLastColumn()).getValues()[0];
   // var pdfBlob = createPDF(questions, responses);
-  var userEmailAddress = responses[questions.length - 1];
+  var userEmailAddress = responses[1];
   // Create an HTML representation of the table
   var htmlTable = "<table style='width: 100%; color: rgb(5, 107, 240); font-family: Calibri, sans-serif; border-collapse: collapse; border: 2px solid rgb(236, 142, 65);'><tbody><tr><th style='width: 50%; border: 2px solid rgb(236, 142, 65);'>Question</th><th style='width: 50%; border: 2px solid rgb(236, 142, 65);'>Response</th></tr>";
 
@@ -13,29 +13,33 @@ function onFormSubmit(e) {
     htmlTable += "<tr><td style='width: 50%; border: 2px solid rgb(236, 142, 65);'>" + questions[i] + "</td><td style='width: 50%; border: 2px solid rgb(236, 142, 65);'>" + responses[i] + "</td></tr>";
   }
   htmlTable += "</tbody></table>";
-
-  sendInternalEmail(htmlTable);
-  sendSubmitterEmail(userEmailAddress, htmlTable);
+  var emailAddresses = ["fmbit@mississaugajamaat.com", "thismail.raja@gmail.com"];
+  sendInternalEmail(emailAddresses, htmlTable, responses);
+  // sendSubmitterEmail(userEmailAddress, htmlTable, "template.html");
 
 }
 
 
-function sendInternalEmail(htmlTable) {
+function sendInternalEmail(emailAddresses, htmlTable, responses) {
   // Email the PDF to a set of email addresses.
-  var emailAddresses = ["fmbpayments@mississaugajamaat.com", "fmbgrocery@mississaugajamaat.com", "fmbpurchase@mississaugajamaat.com", "fmbsecretary@mississaugajamaat.com", "fmbit@mississaugajamaat.com"];
-  // var emailAddresses = ["thismail.raja@gmail.com","hasanalid@gmail.com"];
+  // var emailAddresses = ["fmbaccounts@mississaugajamaat.com", "fmbgrocery@mississaugajamaat.com","fmbpurchase@mississaugajamaat.com", "fmbsecretary@mississaugajamaat.com", "fmbit@mississaugajamaat.com" ];
   var subject = "Fateha Request Form Response PDF";
   var senderName = "FMB-Mississauga"
   var senderEmail = "fmb@mississaugjamaat.com"
-  var htmlBody = "<p>Please find the attached the a New Fateha form responses.</p>";
-  htmlBody += htmlTable;
-  htmlBody += "<p>Shukran</p> FMB Mississauga";
-
+  var apiUrl = "https://script.google.com/a/macros/mississaugajamaat.com/s/AKfycbzfDIHMXITk2M3HpKsMSQ22d8N2_ztPjRHTjX7tyk4/dev"
 
   for (var i = 0; i < emailAddresses.length; i++) {
+    var htmlBody = "";
+    htmlBody = "<p>Please find the attached the a New Fateha form responses.</p>";
+    htmlBody += htmlTable;
+    if (emailAddresses[i] == "fmbit@mississaugajamaat.com") {
+      htmlBody += "<a href='" + apiUrl + "?identifier=" + convertToSimpleDateFormat(responses[0]) + "&status=Approved' style='text-decoration: none;'><button style='background-color: #008CBA; border: none; color: white; padding: 10px 20px; text-align: center; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;'>Approve</button></a>";
+      htmlBody += "<a href='" + apiUrl + "?identifier=" + convertToSimpleDateFormat(responses[0]) + "&status=Denied' style='text-decoration: none;'><button style='background-color: #f44336; border: none; color: white; padding: 10px 20px; text-align: center; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;'>Deny</button></a>";
+    }
+    htmlBody += "<p>Shukran</p> FMB Mississauga";
     var emailData = {
       to: emailAddresses[i],
-      subject: "Thank you for Submitting the form",
+      subject: "New Fateha form responses: " + responses[0],
       body: htmlBody,
       // name: senderName + " <" + senderEmail + ">",
       // attachments: [pdfBlob]
@@ -47,7 +51,7 @@ function sendInternalEmail(htmlTable) {
 }
 
 
-function sendSubmitterEmail(userEmailAddress, htmlTable) {
+function sendSubmitterEmail(userEmailAddress, htmlTable, template) {
   var emailAddresses = [userEmailAddress, "fmbit@mississaugajamaat.com"];
   // var emailAddresses = ["thismail.raja@gmail.com","hasanalid@gmail.com"];
 
@@ -57,7 +61,7 @@ function sendSubmitterEmail(userEmailAddress, htmlTable) {
   }
 
   // Get the HTML template file by its name within the specified folder
-  var templateName = "template.html";
+  var templateName = template;
   var templateFile = folder.getFilesByName(templateName).next();
 
   // Read the HTML template file
--- Scripts/Fateha Form/workflow.js
+++ Scripts/Fateha Form/workflow.js
@@ -1,6 +1,6 @@
 function doGet(request) {
     try {
-        var sheetname = "Form Responses 1";
+        var sheetname = "Form Responses 5";
         var doc = SpreadsheetApp.openById(PropertiesService.getScriptProperties().getProperty("id"));
         var sheet = doc.getSheetByName(sheetname);
 
@@ -8,30 +8,37 @@ function doGet(request) {
             throw new Error("Sheet not found");
         }
 
-        var identifier = request.parameter.identifier; // Assuming identifier is provided as the datetime string
+        var identifier = request.parameter.identifier;
 
         if (!identifier) {
             throw new Error("Identifier parameter missing");
         }
 
         var data = sheet.getDataRange().getValues();
-
+        var headerCol = data[0];
+        var row;
+        var status = request.parameter.status;
         for (var i = 1; i < data.length; i++) {
-            var row = data[i];
-            var cellValue = row[0]; // Assuming the datetime is in the first column (index 0)
+            row = data[i];
+            var cellValue = row[0];    // Assuming the datetime format in the sheet is MM/DD/YYYY HH:mm:ss
+            var simpleDate = convertToSimpleDateFormat(cellValue);
+            Logger.log(simpleDate); // This will log "12/1/2023"
 
-            // Assuming the datetime format in the sheet is MM/DD/YYYY HH:mm:ss
-            var formattedCellValue = Utilities.formatDate(cellValue, Session.getScriptTimeZone(), 'MM/dd/yyyy HH:mm:ss');
-
-            if (formattedCellValue === identifier) {
+            if (simpleDate == identifier) {
                 var rowNum = i + 1;
-                var status = request.parameter.status;
 
-                if (!status) {
-                    throw new Error("Status parameter missing");
+                sheet.getRange(rowNum, 12).setValue(status === "Approved" ? "Approved" : "Denied");
+                // Create an HTML representation of the table
+                var htmlTable = "<table style='width: 100%; color: rgb(5, 107, 240); font-family: Calibri, sans-serif; border-collapse: collapse; border: 2px solid rgb(236, 142, 65);'><tbody><tr><th style='width: 50%; border: 2px solid rgb(236, 142, 65);'>Question</th><th style='width: 50%; border: 2px solid rgb(236, 142, 65);'>Response</th></tr>";
+
+                for (var i = 0; i < row.length - 1; i++) {
+                    htmlTable += "<tr><td style='width: 50%; border: 2px solid rgb(236, 142, 65);'>" + headerCol[i] + "</td><td style='width: 50%; border: 2px solid rgb(236, 142, 65);'>" + row[i] + "</td></tr>";
                 }
+                htmlTable += "</tbody></table>";
 
-                sheet.getRange(rowNum, 11).setValue(status === "Approved" ? "Approved" : "Denied");
+                if (status == "Approved") {
+                    sendSubmitterEmail(row[1], htmlTable, "template_fateha_approval.html")
+                }
 
                 var response = {
                     success: true,
@@ -39,11 +46,14 @@ function doGet(request) {
                 };
                 return ContentService.createTextOutput(JSON.stringify(response)).setMimeType(ContentService.MimeType.JSON);
             }
+
+
         }
+
 
         var notFoundResponse = {
             success: false,
-            message: "Identifier not found"
+            message: identifier + "|" + simpleDate + "Identifier not found"
         };
         return ContentService.createTextOutput(JSON.stringify(notFoundResponse)).setMimeType(ContentService.MimeType.JSON);
 
@@ -56,6 +66,22 @@ function doGet(request) {
     }
 }
 
+function convertToSimpleDateFormat(dateString) {
+    var date = new Date(dateString);
+
+    // Check if the date is valid
+    if (isNaN(date.getTime())) {
+        throw new Error("Invalid date");
+    }
+
+    var month = (date.getMonth() + 1).toString(); // Months are zero-based
+    var day = date.getDate().toString();
+    var year = date.getFullYear();
+
+    var formattedDate = month + '/' + day + '/' + year;
+
+    return formattedDate;
+}
 
 function setup() {
     var doc = SpreadsheetApp.getActiveSpreadsheet();
